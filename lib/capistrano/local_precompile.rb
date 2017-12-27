f2618731b4a63e901397a1393fec83ab6f2e632f
Iván Cortés <ivancortesromero.ali@gmail.com>
2017-12-27 16:00:56 -0500

    Refactor to fix own deploy

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -6,35 +6,19 @@ namespace :load do
     set :assets_dir,       "public/assets"
     set :packs_dir,        "public/packs"    
     set :rsync_cmd,        "rsync -av --delete"
+    set :user,             ENV['DEPLOY_USER'] || 'deploy'
+    set :host,             ENV.fetch('DEPLOY_HOST')
 
     after "bundler:install", "deploy:assets:prepare"
-    #before "deploy:assets:symlink", "deploy:assets:remove_manifest"
-    after "deploy:assets:prepare", "deploy:assets:cleanup"
+    after "deploy:assets:prepare", "deploy:assets:compile"
+    after "deploy:assets:compile", "deploy:assets:clup"
   end
 end
 
 namespace :deploy do
-  # Clear existing task so we can replace it rather than "add" to it.
   Rake::Task["deploy:compile_assets"].clear
 
   namespace :assets do
-
-    # desc "Remove manifest file from remote server"
-    # task :remove_manifest do
-    #   with rails_env: fetch(:assets_dir) do
-    #     execute "rm -f #{shared_path}/#{shared_assets_prefix}/manifest*"
-    #   end
-    # end
-
-    desc "Remove all local precompiled assets"
-    task :cleanup do
-      run_locally do
-        with rails_env: fetch(:precompile_env) do
-          execute "rm -rf", fetch(:assets_dir)
-        end
-      end
-    end
-
     desc "Actually precompile the assets locally"
     task :prepare do
       run_locally do
@@ -46,15 +30,20 @@ namespace :deploy do
     end
 
     desc "Performs rsync to app servers"
-    task :precompile do
-      on roles(fetch(:assets_role)) do
+    task :compile do
+      run_locally do
+        execute "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{fetch(:user)}@#{fetch(:host)}:#{release_path}/#{fetch(:assets_dir)}/"
+        execute  "#{fetch(:rsync_cmd)} ./#{fetch(:packs_dir)}/ #{fetch(:user)}@#{fetch(:host)}:#{release_path}/#{fetch(:packs_dir)}/"
+      end
+    end
 
-        local_manifest_path = run_locally "ls #{assets_dir}/manifest*"
-        local_manifest_path.strip!
-
-        run_locally "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{user}@#{server}:#{release_path}/#{fetch(:assets_dir)}/"
-        run_locally "#{fetch(:rsync_cmd)} ./#{fetch(:packs_dir)}/ #{user}@#{server}:#{release_path}/#{fetch(:packs_dir)}/"  #TODO: Check if exists      
-        run_locally "#{fetch(:rsync_cmd)} ./#{local_manifest_path} #{user}@#{server}:#{release_path}/assets_manifest#{File.extname(local_manifest_path)}"
+    desc "Remove all local precompiled assets"
+    task :clup do
+      run_locally do
+        with rails_env: fetch(:precompile_env) do
+          execute "rm -rf", fetch(:assets_dir)
+          execute "rm -rf", fetch(:packs_dir)
+        end
       end
     end
   end
